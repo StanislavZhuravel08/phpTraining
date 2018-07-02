d047cd0a27825028c463dc0f59a83673af5141ea
StanislavZhuravel08 <stanislavz@default-value.com>
2018-07-02 19:00:59 +0300

    work on validator

--- src/js/Validator.js
+++ src/js/Validator.js
@@ -4,10 +4,22 @@ export default class Validator {
             'validate-int': this.validateInt,
             'validate-range': this.validateRange,
             'validate-required': this.validateRequired
-        }
+        };
     }
 
     validate($form) {
 
+    }
+
+    /**
+     * @param value
+     * @returns {boolean}
+     */
+    validateInt(value) {
+        return Number.isInteger(value);
+    }
+
+    validateRange($form) {
+        let min = $form.find('input').data();
     }
 }
